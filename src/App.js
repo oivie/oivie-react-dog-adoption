1b040ccc17d1e3f0aa615c4826b830c4b67c2a3d
oivie <pashkova.e19@gmail.com>
2023-03-10 14:04:48 +0300

    Initial upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { data } from './data.js';
+
+
 
 function App() {
+  const [ dog, setDog ] = useState(0);
+  const { id, name, description, age, image } = data[ dog ];
+
+
+  const prevDog = () => {
+    setDog((dog => {
+      dog --;
+
+      if (dog < 0) {
+        return data.length - 1;
+      }
+      return dog;
+    }))
+  }
+
+
+  const nextDog = () => {
+    setDog((dog => {
+      dog++;
+
+      if (dog > data.length - 1) {
+        dog = 0;
+      } 
+      return dog;
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div key={ id }>
+      <div className='container'>
+        <img src={ image } />
+      </div>
+
+      <div className='container'>
+        <h1>{ name }</h1>
+      </div>
+
+      <div className='container'>
+        <h3>{ description }</h3>
+      </div>
+
+      <div className='container'>
+        <p> Age: { age }</p>
+      </div>
+
+      <div className='btn container'>
+        <button onClick={ prevDog } >Previous</button>
+        <button onClick={ nextDog }>Next</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,53 @@
+import victor from './victor.jpg';
+import leroy from './leroy.jpg';
+import purma from './purma.jpg';
+import sch from './sch.jpg'
+import tootie from './tootie.jpg';
+
+
+export const data = [
+
+  {
+
+    id: 1,
+    name: "Victor",
+    description: "Unleash your love with a loyal companion.",
+    age: 3,
+    image: victor,
+  },
+
+  {
+    id: 2,
+    name: "Leroy",
+    description: "Open your heart and home to a furry friend in need.",
+    age: 2,
+    image: leroy
+  },
+
+  {
+    id: 3,
+    name: "Tootie",
+    description: "Your new best friend is waiting for you to adopt them.",
+    age: 3,
+    image: tootie,
+  },
+
+  {
+
+    id: 4,
+    name: "Schnitzel",
+    description: "A wagging tail is waiting for you to adopt.",
+    age: 4,
+    image: sch,
+  },
+
+  {
+
+    id: 5,
+    name: "Purma",
+    description: "Your perfect pooch is waiting for you.",
+    age: 3,
+    image: purma,
+  }
+
+]
